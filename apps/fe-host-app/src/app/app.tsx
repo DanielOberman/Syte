f423dc508a2e77f42b1aa918598438fba95311e4
Vitalii Oberman <vitalii.oberman@pwc.ru>
2023-04-22 00:47:43 +0300

    created prettier and eslint config

--- apps/fe-host-app/src/app/app.tsx
+++ apps/fe-host-app/src/app/app.tsx
@@ -1,7 +1,7 @@
 import NxWelcome from './nx-welcome';
 
 export function App() {
-  return <NxWelcome title="fe-host-app" />;
+    return <NxWelcome title="fe-host-app" />;
 }
 
 export default App;
