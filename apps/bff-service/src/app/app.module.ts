fb461a5fb6872aaedc24bfb0fd07ccd4e63cbc9f
Vitalii Oberman <vitalii.oberman@pwc.ru>
2023-04-22 00:34:02 +0300

    created  mongo db config and docker config

--- apps/bff-service/src/app/app.module.ts
+++ apps/bff-service/src/app/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { getMongoConfig } from '../config/db-connect.config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
