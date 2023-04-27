8c71942dd97e8c8f470f13bc23409caefd3e5f0b
Vitalii Oberman <vitalii.oberman@pwc.ru>
2023-04-27 10:55:22 +0300

    add db

--- apps/bff-service-e2e/src/bff-service/bff-service.spec.ts
+++ apps/bff-service-e2e/src/bff-service/bff-service.spec.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 describe('GET /api/client', () => {
     it('should return a message', async () => {
-        const res = await axios.get(`/api`);
+        const res = await axios.get(`/api/client`);
 
         expect(res.status).toBe(200);
         expect(res.data).toEqual({ message: 'Hello API' });
