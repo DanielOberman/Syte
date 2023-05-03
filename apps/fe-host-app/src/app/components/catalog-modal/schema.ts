d39afd92b5742e22c77fabfb0198b9798b9ecb82
Vitalii Oberman <vitalii.oberman@pwc.ru>
2023-05-03 08:34:12 +0300

    fix

--- apps/fe-host-app/src/app/components/catalog-modal/schema.ts
+++ apps/fe-host-app/src/app/components/catalog-modal/schema.ts
@@ -1,7 +1,10 @@
 import * as yup from 'yup';
 
 export const schema = yup.object().shape({
-    name: yup.string().required(),
+    name: yup
+        .string()
+        .required()
+        .matches(/^[a-zA-Z]+$/, 'The catalog name must consist only of letters'),
     vertical: yup.string().required(),
     isPrimary: yup.boolean(),
 });
